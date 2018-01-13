2904ecece38d0705b229bc33c60073ae7b655a38
EC2 Default User <cqwood223@gmail.com>
2018-01-13 21:32:53 +0000

    added application main page

--- getURL.js
+++ getURL.js
@@ -2,7 +2,7 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
 exports.handler = (event, context, callback) => {
-  var params = {Bucket: 'privatecqw', Key: 'index.html', Expires: 6000};
+  var params = {Bucket: 'ctlrbldprivate', Key: 'mainpage.html', Expires: 6000};
   var url = s3.getSignedUrl('getObject', params);
   var resp = {
         "statusCode": 200,

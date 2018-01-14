127445cf747cf31776b7d5f84176119e56d530cf
EC2 Default User <cqwood223@gmail.com>
2018-01-14 10:08:51 +0000

    adding lamba for template creation

---
+++ ctlrbld.py
@@ -0,0 +1,15 @@
+
+
+def handler(event, context):
+    print("In lambda handler")
+
+
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": str(event["body"])
+    }
+    
+    return resp
